af029f4a93c32ffae76c8bcab7d99e9731ada5d2
andrii2706 <andriipas200017@gmail.com>
2025-08-27 20:46:18 +0300

    inital changes for project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsFormPlugin } from '@ngxs/form-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { withNgxsWebSocketPlugin } from '@ngxs/websocket-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+     provideClientHydration(withEventReplay()),
+     provideStore([],
+     withNgxsReduxDevtoolsPlugin(),
+     withNgxsFormPlugin())
+]
 };
