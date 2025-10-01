ddc92148ae47454c39b76ea9e9cf4b55967c80dd
andrii2706 <andriipas200017@gmail.com>
2025-10-01 21:22:28 +0300

    add some changes for developers pages

--- src/app/pages/developers/developers.component.ts
+++ src/app/pages/developers/developers.component.ts
@@ -1,16 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  Injector,
+  OnInit,
+  runInInjectionContext,
+  signal,
+} from '@angular/core';
+import { DevelopersService } from '../../shared/services/developers/developers.service';
+import { Store } from '@ngxs/store';
+import { MainInterface } from '../../shared/models/main.interfaces';
+import { Developers } from '../../shared/models/developers.interface';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { DevelopersCardsComponent } from '../../shared/components/developers-cards/developers-cards.component';
+import { finalize } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-developers',
-  imports: [],
+  imports: [NgxPaginationModule, DevelopersCardsComponent],
   templateUrl: './developers.component.html',
   styleUrl: './developers.component.scss',
 })
 export class DevelopersComponent implements OnInit {
-  constructor() {
-    console.log('constructor');
-  }
+  private store = inject(Store);
+  private developerService = inject(DevelopersService);
+  private injector = inject(Injector);
+  private destroyRef = inject(DestroyRef);
+
+  page: number = 1;
+  cardSkeleton: boolean = false;
+
+  developers = signal<MainInterface<Developers> | null>(null);
+  developersInfo = computed(() => this.developers()?.results ?? []);
+
   ngOnInit(): void {
-    console.log('on init');
+    runInInjectionContext(this.injector, () => {
+      effect(() => {
+        this.developers.set(this.developerService.developers());
+      });
+    });
+  }
+
+  navigateTo(page: number) {
+    this.cardSkeleton = true;
+    this.developerService
+      .getDevelopers(page)
+      .pipe(
+        finalize(() => (this.cardSkeleton = false)),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((developers) => {
+        this.developers.set(developers);
+      });
   }
 }
--- src/app/shared/components/developers-cards/developers-cards.component.ts
+++ src/app/shared/components/developers-cards/developers-cards.component.ts
@@ -6,20 +6,9 @@ import { Developers } from '../../models/developers.interface';
   imports: [],
   templateUrl: './developers-cards.component.html',
   styleUrl: './developers-cards.component.scss',
+  standalone: true,
 })
 export class DevelopersCardsComponent {
   developers = input<Developers>();
-  changeStyle = input<boolean>();
-  genreInfo = output<string>();
   skeleton = input<boolean>();
-
-  ngOnInit() {}
-
-  genres(genre: string) {
-    this.genreInfo.emit(genre);
-  }
-
-  addToWishList(game: Developers | undefined) {
-    console.log(game);
-  }
 }
