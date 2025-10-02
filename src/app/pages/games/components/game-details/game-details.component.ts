9b54fa6edd10e54623a9a85cd8a3b8424e70fcd7
andrii2706 <andriipas200017@gmail.com>
2025-10-02 14:45:29 +0300

    add some ui parts

--- src/app/pages/games/components/game-details/game-details.component.ts
+++ src/app/pages/games/components/game-details/game-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, inject, Injector, OnInit, runInInjectionContext, signal } from '@angular/core';
+import { GamesService } from '../../../../shared/services/games/games.service';
+import { GameDetails } from '../../../../shared/models/games.interfaces';
 
 @Component({
   selector: 'app-game-details',
@@ -8,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
 })
 export class GameDetailsComponent implements OnInit {
+  private injector = inject(Injector);
+  private gamesService = inject(GamesService);
+
+  gameByIdInfo = signal<GameDetails | null>(null)
+
   ngOnInit(): void {
-    console.log('games details');
+    runInInjectionContext(this.injector, () => {
+      effect(() => {})
+    })
+    this.gameByIdInfo.set(this.gamesService.gameById())
+
   }
 }
